82ac362df402d3a6ed0192fbfd5568c299c0e680
Juan Ler <jleruga@gmail.com>
2020-09-25 12:03:58 -0300

    Proyecto Base

---
+++ app/src/main/java/com/fif/fpay/android/fsend/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fif.fpay.android.fsend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
