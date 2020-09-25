73466a68a482e0cd711f5b86e1ce2a8d0438469b
Juan Ler <jleruga@gmail.com>
2020-09-25 13:28:36 -0300

    Lista rancia ViewModel Navigation

---
+++ app/src/main/java/com/fif/fpay/android/fsend/ShipmentList.kt
@@ -0,0 +1,32 @@
+package com.fif.fpay.android.fsend
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+class ShipmentListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ShipmentListFragment()
+    }
+
+    private lateinit var viewModel: ShipmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.shipment_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ShipmentViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/fif/fpay/android/fsend/ShipmentViewModel.kt
@@ -0,0 +1,39 @@
+package com.fif.fpay.android.fsend
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.fif.fpay.android.fsend.commons.AbsentLiveData
+import com.fif.fpay.android.fsend.commons.Event
+import com.fif.fpay.android.fsend.commons.Resource
+import com.fif.fpay.android.fsend.commons.Status
+
+class ShipmentViewModel : ViewModel() {
+    //private val onboardingRepository: IOnboardingRepository = OnboardingRepository()
+
+
+//    var getCustomerType: LiveData<Resource<Any>>
+//
+//
+//    private var customerIdLiveData: MutableLiveData<Any> = MutableLiveData()
+//
+//    var getAskPassword: LiveData<Event<String?>>
+//
+//    init {
+//        getCustomerType = Transformations.switchMap(customerIdLiveData) {
+//            if (it == null)
+//                AbsentLiveData.create()
+//            else {
+//                //onboardingRepository.getClientTypeLiveData(xVersion, it)
+//            }
+//        }
+//
+//        getAskPassword = Transformations.map(getCustomerType) { response ->
+//            when(response.status) {
+//                Status.ERROR -> Event(response.message)
+//                else -> Event(null)
+//            }
+//        }
+//    }
+}
