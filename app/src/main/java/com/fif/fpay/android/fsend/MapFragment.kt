a17cf3e96d3562be5b7fb67a0b711def4efa6a0b
lcunarro <lcunarro@technisys.com>
2020-09-25 23:15:07 -0300

    -maps

--- app/src/main/java/com/fif/fpay/android/fsend/MapFragment.kt
+++ app/src/main/java/com/fif/fpay/android/fsend/MapFragment.kt
@@ -1,10 +1,39 @@
 package com.fif.fpay.android.fsend
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navGraphViewModels
+import com.fif.fpay.android.fsend.data.DirectionResponses
+import com.fif.fpay.android.fsend.fragments.ShipmentListFragment
+import com.fif.fpay.android.fsend.viewmodels.ShipmentViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,44 +45,116 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MapFragment : Fragment(), OnMapReadyCallback , GoogleMap.OnMarkerClickListener{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var map: GoogleMap
+    private lateinit var myPosition: LatLng
+    private var initDirection: String = ""
+    private var goToDirection: String = ""
+    private var zoom: Float = 16.0f
+    private var mapFragment: SupportMapFragment? = null
+
+    private val viewModel: ShipmentViewModel by navGraphViewModels(R.id.nav_graph_shipment)
+
+    companion object {
+        fun newInstance() =
+            ShipmentListFragment()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        myPosition = LatLng(-34.8954889, -58.4001518) //Obtener mi posicion de gps
+        mapFragment = childFragmentManager.findFragmentById(R.id.maps_view) as? SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        map.setOnMarkerClickListener(this)
     }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        map = googleMap!!
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 15.6f))
+
+        initDirection = "Nuestras Malvinas 277,Glew"
+        goToDirection = "Aranguren 242,Glew"
+
+        var uno:LatLng = LatLng(-34.897568, -58.402854)
+        var dos:LatLng = LatLng(-34.897568, -58.402854)
+        var tres:LatLng = LatLng(-34.897568, -58.402854)
+        var cuatro:LatLng = LatLng(-34.897568, -58.402854)
+        var cinco:LatLng = LatLng(-34.897568, -58.402854)
+
+
+        //map.addMarker()
+
+    }
+
+
+    private fun getPolyline(myLocation:LatLng,customerLocation:LatLng){
+
+        val apiServices = RetrofitClient.apiServices(requireContext())
+        apiServices.getDirection(myLocation.toString(), customerLocation.toString(), getString(R.string.api_key))
+            .enqueue(object : Callback<DirectionResponses> {
+                override fun onResponse(call: Call<DirectionResponses>, response: Response<DirectionResponses>) {
+                    drawPolyline(response)
+                    Log.d("bisa dong oke", response.message())
+                }
+
+                override fun onFailure(call: Call<DirectionResponses>, t: Throwable) {
+                    Log.e("anjir error", t.localizedMessage)
+                }
+            })
+    }
+
+    private fun drawPolyline(response: Response<DirectionResponses>) {
+        val shape = response.body()?.routes?.get(0)?.overviewPolyline?.points
+        val polyline = PolylineOptions()
+            .addAll(PolyUtil.decode(shape))
+            .width(8f)
+            .color(Color.RED)
+
+        val markerInit = MarkerOptions()
+            .position(polyline.points[0])
+            .title(initDirection)
+
+        val markerGoTo = MarkerOptions()
+            .position(polyline.points[polyline.points.size -1])
+            .title(goToDirection)
+
+
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(polyline.points[(polyline.points.size-1)/2], zoom-((polyline.points.size/2).toFloat())))
+        map.addPolyline(polyline)
+        map.addMarker(markerInit)
+        map.addMarker(markerGoTo)
+    }
+
+    private interface ApiServices {
+        @GET("maps/api/directions/json")
+        fun getDirection(@Query("origin") origin: String,
+                         @Query("destination") destination: String,
+                         @Query("key") apiKey: String): Call<DirectionResponses>
+    }
+
+    private object RetrofitClient {
+        fun apiServices(context: Context): ApiServices {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(context.resources.getString(R.string.base_url))
+                .build()
+
+            return retrofit.create<ApiServices>(ApiServices::class.java)
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        getPolyline(myPosition,marker!!.position)
+        return true
+    }
+
+
 }
