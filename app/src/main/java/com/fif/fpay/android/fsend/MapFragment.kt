502fe6d425280a0bba7990b2d11d8d5072bbc150
lcunarro <lcunarro@technisys.com>
2020-09-25 23:40:47 -0300

    -maps

--- app/src/main/java/com/fif/fpay/android/fsend/MapFragment.kt
+++ app/src/main/java/com/fif/fpay/android/fsend/MapFragment.kt
@@ -41,6 +41,8 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
+ *
+ *
  * A simple [Fragment] subclass.
  * Use the [MapFragment.newInstance] factory method to
  * create an instance of this fragment.
