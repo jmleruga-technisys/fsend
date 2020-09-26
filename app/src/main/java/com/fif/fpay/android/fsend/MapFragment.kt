9f17ad98aeee1e8a7366d727b5e4395f0763ba8e
lcunarro <lcunarro@technisys.com>
2020-09-25 23:39:27 -0300

    -maps

--- app/src/main/java/com/fif/fpay/android/fsend/MapFragment.kt
+++ app/src/main/java/com/fif/fpay/android/fsend/MapFragment.kt
@@ -40,8 +40,6 @@ import retrofit2.http.Query
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-
-
 /**
  * A simple [Fragment] subclass.
  * Use the [MapFragment.newInstance] factory method to
